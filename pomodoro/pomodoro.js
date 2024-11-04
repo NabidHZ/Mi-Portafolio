4685413dd17e1e79fcbb76216a296ab2e3836775
Nahaza2425 <nabidhassan34@gmail.com>
2024-11-04 17:19:59 +0100

    funciones render

--- pomodoro/pomodoro.js
+++ pomodoro/pomodoro.js
@@ -18,5 +18,25 @@ const form = document.querySelector('#form');
 //se ejecuta una funcion cuendo se activa submit
 form.addEventListener('submit', e =>{
     e.preventDefault(); //para que no se envie directamente
-    
+    if(itTask.value != ''){ //Si itTask es diferente de un string vacio
+        createTask(itTask.value);
+        itTask.value='';//esto vacia el  input para la suiente tarea
+        rederTask();//llama a la funcion
+    }
+
 });
+
+//funcion crear la tarea
+function createTask(){
+    
+    const newTask ={
+        //id dinamico,math.ramdom me da un valor decimal, lo elimino con toString slice me quita los 3 caracteres inciales
+        id: (Math.random() * 100).toString(36).slice(3), 
+        title: value,
+        completed: false,
+    };
+
+    //agrego newTask al arreglo task[]
+    task.unshift(newTask);
+
+}
