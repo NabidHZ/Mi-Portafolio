436f575c038cdad91d74873dfa8a25f0a090ce3b
Nahaza2425 <nabidhassan34@gmail.com>
2025-05-01 18:43:05 +0200

    Subir proyecto Spring

--- Videoclub Java(SpringBoot) + SQL,MongoDb/Peliculas Clot/src/main/java/com/clotfilms/repository/PeliculaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.clotfilms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.clotfilms.model.Pelicula;
-
-public interface PeliculaRepository extends JpaRepository<Pelicula, Long> {
-    // Métodos personalizados si son necesarios
-}
